ebba0c857416ba4e13345a7592a8bbd40b876df1
jungho5045 <jungho5045@naver.com>
2020-01-05 17:22:59 +0900

    [refactoring] "Chapter 09 인터페이스의 요소들1 수강"

--- Chapter9/src/interfaceex/calc.java
+++ Chapter9/src/interfaceex/calc.java
@@ -10,4 +10,28 @@ public interface calc {
 	int times(int num1, int num2);
 	int divide(int num1, int num2);
 	
+	default void description() {
+		System.out.println("정수 계산기를 구현합니다.");
+		// myMethod();
+	}
+	
+	static int total(int[] arr) {
+		int total = 0;
+		
+		for(int i: arr) {
+			total += i;
+		}
+		
+		return total;
+	}
+	
+	// java 9부터 재공
+//	private void myMethod() {
+//		System.out.println("private method");
+//	}
+//	
+//	private static void mystaticMethod() {
+//		System.out.println("private static method");
+//	}
+	
 }
