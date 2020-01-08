89da6306e3c79947f4c73c2c407e694efe987141
jungho5045 <jungho5045@naver.com>
2020-01-09 00:43:54 +0900

    [refactor] "Chapter12 내부클래스 재수강 완료"

--- Chapter12/src/innerclass/InnerTest.java
+++ Chapter12/src/innerclass/InnerTest.java
@@ -2,18 +2,18 @@ package innerclass;
 
 class OutClass{
 	
-	private int num = 10;
-	private static int sNum = 20;
-	private InClass inClass;
+	private int num = 10;				// private 접근지정자 : 클래스 내부에서만 접근가능
+	private static int sNum = 20;		// static 변수 : 정적 타입의 변수로, 인스턴스 생성전에 메모리에 할당된다.(고정)
+	private InClass inClass;			// InClass 타입의 inClass 객체 선언
 	
-	public OutClass(){
-		inClass = new InClass();
+	public OutClass(){					// 생성자에 InnerClass 생성을 구현한다.
+		inClass = new InClass();		// InClass 타입으로 선언한 inClass 객체를 InClass() 생성자를 이용하여 생성한다.
 	}
 	
 	class InClass{						// OutClass 생성 후 사용
-		int iNum = 100;
+		int iNum = 100;					// 멤버 변수
 		
-		void inTest() {
+		void inTest() {					// 메서드
 			System.out.println(num);
 			System.out.println(sNum);
 		}
@@ -57,7 +57,7 @@ public class InnerTest {
 		
 		// OutClass.InStaticClass sInClass = new OutClass.InStaticClass();	// OutClass 선언없이, InClass에 있은 static 클래스를 바로 생성할 수 있다.
 		
-		OutClass.InStaticClass.sTest();	// InClass에 있는 static class
+		OutClass.InStaticClass.sTest();	// 외부에서 static class 바로 접근하는 방법
 		
 	}
 
