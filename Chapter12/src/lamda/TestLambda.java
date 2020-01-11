8a4123920387e05bd0500a5d6624d754c68141d1
jungho5045 <jungho5045@naver.com>
2020-01-11 18:30:49 +0900

    [refactor] "Chapter12 람다식 함수형 프로그래밍"

--- Chapter12/src/lamda/TestLambda.java
+++ Chapter12/src/lamda/TestLambda.java
@@ -8,7 +8,7 @@ public class TestLambda {
 
 	public static void main(String[] args) {
 		
-		PrintString lambdStr = s->System.out.println(s);
+		PrintString lambdStr = s->System.out.println(s);	// 매개변수로 s를 사용하고, 기능은 전달받은 s를 출력하는 기능을 구현하여 람다식을 사용한다.
 		lambdStr.showString("Test");
 		
 		showMyString(lambdStr);
